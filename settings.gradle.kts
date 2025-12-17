08e50d90026b9542368091c7680419ce8a22380b
Thanh Nguyen <tnguy247@gmu.edu>
2025-12-17 18:36:56 -0500

    init

---
+++ settings.gradle.kts
@@ -0,0 +1,1 @@
+rootProject.name = "nct-review-back"
---
+++ src/main/kotlin/com/example/nctreviewback/NctReviewBackApplication.kt
@@ -0,0 +1,11 @@
+package com.example.nctreviewback
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class NctReviewBackApplication
+
+fun main(args: Array<String>) {
+    runApplication<NctReviewBackApplication>(*args)
+}
